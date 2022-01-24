c9c6bc4e341f77edde8a08ed365b9436e7870205
vishal sam <vinishakr9@gmail.com>
2022-01-24 13:59:47 +0530

    Added power of two logic

---
+++ Poweroftwo.java
@@ -0,0 +1,17 @@
+package com.bridfgelabz.basic;
+
+import com.bridfgelabz.utility.Utility;
+import java.util.Scanner;
+
+
+public class Poweroftwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter a number:");
+		int Value = scanner.nextInt();
+		Utility.Poweroftwo(Value);
+	}
+
+}
