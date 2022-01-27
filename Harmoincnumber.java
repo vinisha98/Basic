058c45ef4b91f4330c4a14c002c0fbbfac0a968a
vishal sam <vinishakr9@gmail.com>
2022-01-27 18:40:57 +0530

    Added leapyear logic

---
+++ Harmoincnumber.java
@@ -0,0 +1,16 @@
+package com.bridfgelabz.basic;
+
+import com.bridfgelabz.utility.Utility;
+import java.util.Scanner;
+
+public class Harmoincnumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter a number:");
+		int Value = scanner.nextInt();
+		Utility.Harmonicnumber(Value);
+	}
+
+}
